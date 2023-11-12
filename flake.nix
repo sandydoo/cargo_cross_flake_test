13415314a9e83b313e3786bf466b83c2bd214811
Sander <hey@sandydoo.me>
2023-11-12 15:05:23 +0000

    Format

--- flake.nix
+++ flake.nix
@@ -10,74 +10,76 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self,
+  outputs = inputs @ {
+    self,
     nixpkgs,
     rust-overlay,
     devenv,
     fenix,
     flake-utils,
     ...
-  }: 
-  flake-utils.lib.eachSystem ["x86_64-linux" "aarc64-linux" ] (system: let
-    mkCargoCross = pkgs: pkgs.cargo-cross.overrideAttrs (drv: rec {
-        src = pkgs.fetchFromGitHub {
-          owner = "cross-rs";
-          repo = "cross";
-          rev = "44011c8854cb2eaac83b173cc323220ccdff18ea";
-          hash = "sha256-yGYs0691CPFUX9Wg/gkm6PXCX0GnfaKDyL+BCbCUrfw=";
-        };
-        cargoDeps = drv.cargoDeps.overrideAttrs (pkgs.lib.const {
-          inherit src;
-          outputHash = "sha256-Egq2+VVl6ekReoEK2k0Esz7B/zycKQan+um+c3DhbbU=";
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarc64-linux"] (system: let
+      mkCargoCross = pkgs:
+        pkgs.cargo-cross.overrideAttrs (drv: rec {
+          src = pkgs.fetchFromGitHub {
+            owner = "cross-rs";
+            repo = "cross";
+            rev = "44011c8854cb2eaac83b173cc323220ccdff18ea";
+            hash = "sha256-yGYs0691CPFUX9Wg/gkm6PXCX0GnfaKDyL+BCbCUrfw=";
+          };
+          cargoDeps = drv.cargoDeps.overrideAttrs (pkgs.lib.const {
+            inherit src;
+            outputHash = "sha256-Egq2+VVl6ekReoEK2k0Esz7B/zycKQan+um+c3DhbbU=";
+          });
         });
-      });
-  in {
-    devShells = {
-      default = 
-        let
+    in {
+      devShells = {
+        default = let
           pkgs = import nixpkgs {
             inherit system;
             overlays = [rust-overlay.overlays.default];
           };
-	  cargo-cross = mkCargoCross pkgs;
+          cargo-cross = mkCargoCross pkgs;
           toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
-        in pkgs.mkShell {
-        packages = [
-          pkgs.docker
-          pkgs.rustup
-          cargo-cross
-          toolchain
-        ];
-      };
+        in
+          pkgs.mkShell {
+            packages = [
+              pkgs.docker
+              pkgs.rustup
+              cargo-cross
+              toolchain
+            ];
+          };
 
-      devenv =
-        let 
-	  pkgs = nixpkgs.legacyPackages.${system};
-	  cargo-cross = mkCargoCross pkgs;
-	  toolchain = 
-            fenix.packages.${system}.fromToolchainFile {
-              file = ./toolchain.toml;
-	      sha256 = "sha256-0d/UxN6sekF+iQtebQl6jj/AQiT18Uag3CKbsCxc1E0=";
-            };
-
-          # (with fenix.packages.${system}; 
+        devenv = let
+          pkgs = nixpkgs.legacyPackages.${system};
+          cargo-cross = mkCargoCross pkgs;
+          toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./toolchain.toml;
+            sha256 = "sha256-0d/UxN6sekF+iQtebQl6jj/AQiT18Uag3CKbsCxc1E0=";
+          };
+          # (with fenix.packages.${system};
           #   combine [ latest.rustc latest.cargo latest.rust-src targets.x86_64-pc-windows-gnu.latest.rust-std ]);
-	in devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [{
-          packages = [ 
-            pkgs.rustup
-            pkgs.docker
-            cargo-cross
-	    toolchain
-          ];
-          languages.rust.enable = true;
-          languages.rust.channel = "nightly";
-          languages.rust.components = [];
-          languages.rust.toolchain = pkgs.lib.mkForce toolchain;
-          env.RUST_BACKTRACE = "1";
-        }];
+        in
+          devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = [
+                  pkgs.rustup
+                  pkgs.docker
+                  cargo-cross
+                  toolchain
+                ];
+                languages.rust.enable = true;
+                languages.rust.channel = "nightly";
+                languages.rust.components = [];
+                languages.rust.toolchain = pkgs.lib.mkForce toolchain;
+                env.RUST_BACKTRACE = "1";
+              }
+            ];
+          };
       };
-    };
-  });
+    });
 }
